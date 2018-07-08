36ebb99270ad2eb806ef382e91098a2fc11934d8
Donavon Buchanan <donavon@donavons-macbook-pro-2.local>
2018-07-08 03:15:21 -0400

    updated app id for OpenWeatherMap

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "5f22a9655272a72736cff5372bb14d80"
     
 
     //TODO: Declare instance variables here
